23aae3433adcf0d1bab68695417792bccf0cf82f
raiki02 <Raiki_Sakura@mails.ccnu.edu.cn>
2025-12-23 11:49:23 +0800

    feat: add receiver for feed

--- api/req/interaction.go
+++ api/req/interaction.go
@@ -2,5 +2,6 @@ package req
 
 type InteractionReq struct {
 	TargetID string `json:"targetid" binding:"required"`
+	Receiver string `json:"receiver" binding:"required"`
 	Subject  string `json:"subject"`
 }
